35355727be42f67b29c923681508a39894495b96
Sam Singer <samuel.singer1@gmail.com>
2017-11-30 16:28:47 -0700

    Iteration-0 Pass

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,17 @@
 export default class DistrictRepository {
+  constructor(dataPassedIn) {
+    this.data = this.cleanUpData(dataPassedIn);
+    // console.log(this.data);
+  }
 
+  cleanUpData(rawData) {
+    return rawData.reduce( (acc, dataObject) => {
+      // console.log(dataObject);
+      if(!acc[dataObject.Location]) {
+        acc[dataObject.Location] = [];
+      }
+      acc[dataObject.Location].push(dataObject)
+      return acc;
+    }, {})
+  }
 }
