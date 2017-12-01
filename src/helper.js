17f1cc89d6d158c6604be2af4356320e34422962
Sam Singer <samuel.singer1@gmail.com>
2017-11-30 17:54:40 -0700

    Iteration-1-p1 Pass

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,26 @@
 export default class DistrictRepository {
   constructor(dataPassedIn) {
     this.data = this.cleanUpData(dataPassedIn);
-    // console.log(this.data);
   }
 
   cleanUpData(rawData) {
     return rawData.reduce( (acc, dataObject) => {
-      // console.log(dataObject);
       if(!acc[dataObject.Location]) {
-        acc[dataObject.Location] = [];
+        acc[dataObject.Location] = { location: dataObject.Location.toUpperCase(), data: {} };
       }
-      acc[dataObject.Location].push(dataObject)
+      acc[dataObject.Location].data[dataObject.TimeFrame] = Math.round(1000 * dataObject.Data) / 1000 || 0;
       return acc;
     }, {})
   }
+
+  findByName(location = undefined) {
+    if(location) {
+      const dataKeys = Object.keys(this.data)
+      const matchLocation = dataKeys.find( district => {
+        return district.toUpperCase() === location.toUpperCase();
+      })
+      return this.data[matchLocation];
+    }
+  }
+
 }
